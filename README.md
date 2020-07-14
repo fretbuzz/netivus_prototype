9a5ada65aafdbd0f663d1d05d910b60b4a7213db
fretbuzz <jseverin@andrew.cmu.edu>
2020-07-14 15:46:06 -0400

    added some baseline functionality for getting batfish to work...

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-## place holder ##
+## setup instrunctions ##
+
+install + start the batfish docker container
+
+```
+docker pull batfish/allinone
+
+docker run --name batfish -v batfish-data:/data -p 8888:8888 -p 9997:9997 -p 9996:9996 batfish/allinone
+```
+
+Then install pybatfish 
+```
+python3 -m pip install --upgrade pybatfish
+```
+
+Finally, you are ready to run main.py (maybe some other dependency problems, but can install them w/ pip trivially)
--- main.py
+++ main.py
@@ -1,6 +1,59 @@
+import docker, os
 
-def main():
-    pass
+import pandas as pd
+from pybatfish.client.commands import *
+from pybatfish.datamodel import *
+from pybatfish.datamodel.answer import *
+from pybatfish.datamodel.flow import *
+from pybatfish.question import *
+from pybatfish.question import bfq
+
+def main(NETWORK_NAME, SNAPSHOT_NAME, SNAPSHOT_PATH):
+    run_batfish(NETWORK_NAME, SNAPSHOT_NAME, SNAPSHOT_PATH)
+
+def run_batfish(NETWORK_NAME, SNAPSHOT_NAME, SNAPSHOT_PATH):
+    #% run startup.py
+    #bf_session.host = "172.0.0.1"  # <batfish_service_ip>
+    bf_session.host = 'localhost'
+    bf_session.port = '9996'
+
+    bf_set_network(NETWORK_NAME)
+    bf_init_snapshot(SNAPSHOT_PATH, name=SNAPSHOT_NAME, overwrite=True)
+
+    load_questions()
+
+    print(list_questions())
+
+    # vxlanEdges, layer3Edges
+
+    pd.options.display.max_columns = 6
+
+    parse_status = bfq.fileParseStatus().answer().frame()
+
+    print("----------------------")
+
+    print(parse_status)
+
+    print("----------------------")
+
+    parse_warning = pd.DataFrame(bfq.parseWarning().answer().frame())
+
+    print("----------------------")
+
+    print(parse_warning)
+
+    print("----------------------")
+
+    node_properties_trunc = bfq.nodeProperties(properties="Device_Type,Interfaces").answer().frame()
+
+    print(node_properties_trunc)
+
+
 
 if __name__ == "__main__":
-    main()
+    # Initialize a network and snapshot
+    NETWORK_NAME = "example_network"
+    SNAPSHOT_NAME = "example_snapshot"
+    SNAPSHOT_PATH = "./scenarios/Access port config"
+
+    main(NETWORK_NAME, SNAPSHOT_NAME, SNAPSHOT_PATH)
